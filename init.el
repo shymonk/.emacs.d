21e2edf3de638185d22d3fc87469885f56dfa02c
shymonk <hellojohn201@gmail.com>
2014-06-05 21:59:55 +0800

    Refactoring

--- init.el
+++ init.el
@@ -24,6 +24,13 @@
                                            nil 'fullboth)))
 (global-set-key [(meta return)] 'toggle-fullscreen)
 
+;; org-mode
+(setq org-log-done 'time)
+(defun todo ()
+  (interactive)
+  (find-file "~/org/todo.org")
+  )
+
 ;; config for *nix and windows platform
 
 (setq inhibit-startup-message t)
@@ -51,7 +58,6 @@
 (setq column-number-mode t)
 (setq line-number-mode t)
 (setq display-time-24hr-format t)
-(setq display-time-day-and-date t)
 (display-time)
 (set-fontset-font "fontset-default" 'gb18030'("Hei" . "unicode-bmp"))
 (global-auto-revert-mode)
@@ -78,11 +84,11 @@
 (setq sr-speedbar-auto-refresh nil)
 
 ;; fci
-(add-to-list 'load-path "~/.emacs.d/plugins/fci")
-(require 'fill-column-indicator)
-(setq fci-rule-width 1)
-(setq fci-rule-color "SlateGrey")
-(define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+;; (add-to-list 'load-path "~/.emacs.d/plugins/fci")
+;; (require 'fill-column-indicator)
+;; (setq fci-rule-width 1)
+;; (setq fci-rule-color "SlateGrey")
+;; (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
 ;; (global-fci-mode 1)  
 
 ;; bookmark plus
@@ -263,11 +269,11 @@
  '(speedbar-update-flag nil)
  '(tool-bar-mode nil))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "darkslategrey" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 143 :width normal :foundry "outline" :family "Consolas"))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "darkslategrey" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Monaco"))))
  '(whitespace-space ((((class color) (background dark)) (:background "darkslategrey" :foreground "darkgray")))))
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
