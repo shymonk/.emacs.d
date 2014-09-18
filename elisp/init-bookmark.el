58df7f527a3be90ae7d13dd74dab18b678c9514a
shymonk <hellojohn201@gmail.com>
2014-09-18 19:04:38 +0800

    Add bm package, a visible bookmark

--- elisp/init-bookmark.el
+++ elisp/init-bookmark.el
@@ -1,11 +1,16 @@
 ;; bookmark plus
 
 (require-package 'bookmark+)
+(require-package 'bm)
+
 
 (require 'bookmark+)
 (setq bmkp-auto-light-when-set 'Any bookmark)
 
-(global-set-key [(control f2)] 'bookmark-set)
-(global-set-key [f2] 'bmkp-next-bookmark-this-file/buffer-repeat)
+(require 'bm)
+(global-set-key (kbd "<C-f2>") 'bm-toggle)
+(global-set-key (kbd "<f2>")   'bm-next)
+(global-set-key (kbd "<S-f2>") 'bm-previous)
+
 
 (provide 'init-bookmark)
