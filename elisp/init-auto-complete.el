915c6dd53c0dc9408cd16e5bdb9532ba0a6abba5
shymonk <hellojohn201@gmail.com>
2014-11-16 16:21:30 +0800

    Add some blanks

--- elisp/init-auto-complete.el
+++ elisp/init-auto-complete.el
@@ -1,17 +1,21 @@
 ;; Auto-complete
 
 (require-package 'auto-complete)
-
 (require 'auto-complete-config)
+
 (global-auto-complete-mode t)
 (setq-default ac-sources '(ac-source-words-in-same-mode-buffers))
+
 (add-hook 'emacs-lisp-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-symbols)))
 (add-hook 'auto-complete-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-filename)))
+
 (set-face-background 'ac-candidate-face "lightgray")
 (set-face-underline 'ac-candidate-face "darkgray")
-(set-face-background 'ac-selection-face "steelblue") 
+(set-face-background 'ac-selection-face "steelblue")
+
 (define-key ac-completing-map "\M-n" 'ac-next)
 (define-key ac-completing-map "\M-p" 'ac-previous)
+
 (setq ac-auto-start 2)
 (setq ac-dwim t)
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
