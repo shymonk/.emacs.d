67fa92629c693d2351fe086516f54dd3fb8d341f
shymonk <hellojohn201@gmail.com>
2014-12-06 00:04:17 +0800

    Delete trailing whitespace

--- elisp/init-binding.el
+++ elisp/init-binding.el
@@ -4,7 +4,7 @@
 (setq mac-option-modifier nil)
 
 ;; Global key-bind
-(global-set-key [f3] 'toggle-read-only) 
+(global-set-key [f3] 'toggle-read-only)
 (global-set-key [f4] 'kill-this-buffer)
 
 (global-set-key [f6] 'replace-string)
@@ -12,7 +12,7 @@
 (global-set-key [(shift f9)] 'bookmark-set)
 
 (global-set-key [f10] 'python-shell)
-(global-set-key [f11] 'term) 
+(global-set-key [f11] 'term)
 (global-set-key [f12] 'save-buffer);
 
 (global-set-key [(meta ?/)] 'hippie-expand)
