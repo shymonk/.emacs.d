10467d3392a842ed380461104a2f29f6fb728e13
shymonk <hellojohn201@gmail.com>
2015-01-22 22:13:00 +0800

    ignore elpa dir for fiplr

--- elisp/init-fiplr.el
+++ elisp/init-fiplr.el
@@ -2,7 +2,7 @@
 
 (require-package 'fiplr)
 
-(setq fiplr-ignored-globs '((directories (".git" ".svn"))
+(setq fiplr-ignored-globs '((directories (".git" ".svn" "elpa"))
                             (files ("*.pyc" "*.jpg" "*.png" "*.zip" "*~"))))
 
 (global-set-key (kbd "M-`") 'fiplr-find-file)
