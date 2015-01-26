55c618d09d9ff5f9326735cf92043fd4bc72c13d
shymonk <hellojohn201@gmail.com>
2015-01-26 19:55:15 +0800

    Turn off linum-mode for text/xml/eshell mode

--- elisp/init-linum.el
+++ elisp/init-linum.el
@@ -5,4 +5,11 @@
 (require 'linum)
 (global-linum-mode 1)
 
+(defun turn-off-linum-mode ()
+  "Turn off linum-mode"
+  (linum-mode 0)
+  )
+(add-hook 'text-mode-hook 'turn-off-linum-mode)
+(add-hook 'xml-mode-hook 'turn-off-linum-mode)
+
 (provide 'init-linum)
