b439c0bd54b6dd51f807c72a732528c6a14be3e1
shymonk <hellojohn201@gmail.com>
2015-02-12 14:52:03 +0800

    C-c m for bm-toggle

--- elisp/init-bookmark.el
+++ elisp/init-bookmark.el
@@ -9,7 +9,7 @@
 
 (require 'bm)
 (setq bm-highlight-style 'bm-highlight-only-fringe)
-(global-set-key (kbd "<C-f2>") 'bm-toggle)
+(global-set-key (kbd "C-c m") 'bm-toggle)
 (global-set-key (kbd "<f2>")   'bm-next)
 (global-set-key (kbd "<S-f2>") 'bm-previous)
 
