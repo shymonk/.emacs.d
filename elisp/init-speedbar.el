3d1aebcce8caecf8536f73ec4db95126437c7bc6
shymonk <hellojohn201@gmail.com>
2015-11-17 10:59:18 +0800

    Key binding for changing root dir of speedbar

--- elisp/init-speedbar.el
+++ elisp/init-speedbar.el
@@ -8,10 +8,16 @@
 (setq sr-speedbar-right-side nil)
 (setq sr-speedbar-width 25)
 (setq sr-speedbar-skip-other-window-p t)
-(setq sr-speedbar-auto-refresh t)
+(setq sr-speedbar-auto-refresh nil)
 (setq speedbar-show-unknown-files t)
 ;; (setq sr-speedbar-max-width 25)
 
+(defun sr-speedbar-change-root (path)
+  "Change root to PATH."
+  (interactive "DDirectory: ")
+  (projectile-speedbar-project-refresh path)
+  )
+(define-key speedbar-mode-map (kbd "C-c C-c") 'sr-speedbar-change-root)
 (global-set-key [f5] 'sr-speedbar-toggle)
 
 (provide 'init-speedbar)
