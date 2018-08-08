28e6fed19bfd6f2567e4fc85266fc1676bcb7a5d
郭秋实 <guoqiushi@mobike.com>
2018-08-08 20:10:01 +0800

    update

---
+++ elisp/init-go.el
@@ -0,0 +1,29 @@
+;; https://github.com/nsf/gocode
+;; https://github.com/dominikh/yasnippet-go
+
+;; (define-key ac-mode-map (kbd "TAB") 'auto-complete)
+
+(require 'go-mode)
+(require 'go-add-tags)
+(require 'go-eldoc)
+;; (require 'go-autocomplete)
+;; (require 'auto-complete-config)
+(require 'golint)
+
+
+(autoload 'go-mode "go-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+
+
+;; (ac-config-default)
+(defun go-mode-setup ()
+  (go-eldoc-setup)
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (set (make-local-variable 'company-backends) '(company-go))
+  (local-set-key (kbd "C-c .") 'godef-jump))
+(add-hook 'go-mode-hook 'go-mode-setup)
+(add-hook 'go-mode-hook 'company-mode)
+
+(provide 'init-go)
+
---
+++ elisp/init-java.el
@@ -0,0 +1,11 @@
+(require-package 'protobuf-mode)
+
+
+;;----------------------------------------------------------------------------
+;; protobuf-mode
+;;----------------------------------------------------------------------------
+(require 'protobuf-mode)
+(add-to-list 'auto-mode-alist '("\\.proto\\'" . protobuf-mode))
+
+
+(provide 'init-java)
